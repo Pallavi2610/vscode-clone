9a76dec1bb748e2368d5f27fa414d6b80fe15a76
Pallavi2610 <pallavipatle2610@gmail.com>
2024-03-18 12:15:21 +0530

    first commit

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+ const fs=require("fs");
+  const path=require("path");
+  const globalpath=path.join(__dirname,"../","public","uploads");
+ 
+router.get('/', function(req, res, next) {
+  const files =fs.readdirSync(globalpath);
+  res.render('index', { files:files,filedata:""});
+});
+
+router.get('/file/:filename', function(req, res, next) {
+  const filedata=fs.readFileSync(
+    path.join(globalpath,req.params.filename),
+    "utf-8"
+  );
+  const files =fs.readdirSync(globalpath);
+  res.render('index', { files:files,filedata:filedata});
+});
+
+router.post("/createfile",function(req,res,next){
+  // const filenmae=req.body.filename;
+  const {filename}=req.body;
+fs.writeFileSync(path.join(globalpath,filename),"");
+res.redirect(`file/${filename}`);
+
+});
+
+
+module.exports = router;
